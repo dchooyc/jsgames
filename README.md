793edfb2dcb5455805147e47beae9836b690d8b6
Daniel Choo <dchooyc@gmail.com>
2023-11-20 08:59:56 +0800

    Add sprite game

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS game dev tutorials
+Following this [youtube course](https://youtu.be/GFO_txvwK_c)
---
+++ sprite/script.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 1;
+let staggerFrames = 2;
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    
+    if (gameFrame == 0) {
+        frameX++;
+        frameX %= 7;
+    }
+    gameFrame++;
+    gameFrame %= staggerFrames;
+
+    requestAnimationFrame(animate);
+};
+
+animate();
